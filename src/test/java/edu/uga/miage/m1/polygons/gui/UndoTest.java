b746c870a66bf17a5711336a8c13ebe51baad26d
mamoune.bellatif <mamoune.bellatif@businessdecision.com>
2022-11-18 23:52:26 +0100

    test

---
+++ src/test/java/edu/uga/miage/m1/polygons/gui/UndoTest.java
@@ -0,0 +1,16 @@
+package edu.uga.miage.m1.polygons.gui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UndoTest {
+
+    @Test
+    void push() {
+    }
+
+    @Test
+    void undo() {
+    }
+}
