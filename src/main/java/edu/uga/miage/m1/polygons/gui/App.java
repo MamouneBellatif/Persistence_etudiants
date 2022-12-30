6fc422f2e438fbc119c53336196a62d78b6babda
mamoune.bellatif <mamoune.bellatif@businessdecision.com>
2022-12-30 01:35:58 +0100

    file choose export

--- src/main/java/edu/uga/miage/m1/polygons/gui/App.java
+++ src/main/java/edu/uga/miage/m1/polygons/gui/App.java
@@ -2,44 +2,85 @@ package edu.uga.miage.m1.polygons.gui;
 
 import edu.uga.miage.m1.polygons.gui.persistence.JSonVisitor;
 import edu.uga.miage.m1.polygons.gui.persistence.XMLVisitor;
-import edu.uga.miage.m1.polygons.gui.shapes.Circle;
-import edu.uga.miage.m1.polygons.gui.shapes.Square;
-import edu.uga.miage.m1.polygons.gui.shapes.Triangle;
 
 import javax.json.*;
 import java.io.*;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.swing.*;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.commons.io.FilenameUtils;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import com.google.gson.*;
+import javax.swing.filechooser.FileFilter;
+
 
 /**
  * Hello world!
- * 
+ *
  * @author <a href="mailto:christophe.saint-marcel@univ-grenoble-alpes.fr">Christophe</a>
  *
  */
-public class App 
+public class App
 {
-    
+
     public static void jsonToFile(String json) {
-        String path = "export.json";
+        String path = null;
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("Sauvegarder un fichier Json");
+        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        chooser.setAcceptAllFileFilterUsed(false);
+        PrettyUtils.strictFileFilter(chooser, "json");
+        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            path = chooser.getSelectedFile().getAbsolutePath();
+        }
+        // force la bonne extension
+        path = FilenameUtils.removeExtension(path) + ".json";
         try (PrintWriter out = new PrintWriter(new FileWriter(path))) {
-            out.write(json.toString());
+            out.write(PrettyUtils.jsonPrettify(json));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void xmlToFile(String xml) {
-        String path = "export.xml";
-        try (PrintWriter out = new PrintWriter(new FileWriter(path))) {
-            out.write(xml.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    public static JsonObject shapeJsonBuilder(ArrayList<JSonVisitor> arrayList) {
+public static void xmlToFile(String xml) throws ParserConfigurationException, IOException, TransformerException, SAXException {
+
+        xml = PrettyUtils.xmlStringPrettify(xml);
+
+            JFileChooser chooser = new JFileChooser();
+            String path = null;
+            chooser.setDialogTitle("Sauvegarde xml");
+            chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            chooser.setAcceptAllFileFilterUsed(false);
+            PrettyUtils.strictFileFilter(chooser, "xml");
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                path = chooser.getSelectedFile().getAbsolutePath();
+            }
+            // force la bonne extension
+            path = FilenameUtils.removeExtension(path) + ".xml";
+                try (PrintWriter out = new PrintWriter(new FileWriter(path))) {
+                    out.write(xml.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+    public static void shapeJsonBuilder(ArrayList<JSonVisitor> arrayList) {
         StringBuilder jsonString = new StringBuilder("{ \"shapes\":[");
         for (JSonVisitor jv: arrayList){
             jsonString.append(jv.getRepresentation());
@@ -49,21 +90,18 @@ public class App
         }
         jsonString.append("] }");
         App.jsonToFile(jsonString.toString());
-        JsonReader jsonReader = Json.createReader(new StringReader(jsonString.toString()));
-        System.out.println(jsonReader.toString());
-        return jsonReader.readObject();
     }
 
-    public static String xmlBuilder(ArrayList<XMLVisitor> arrayList) {
-        StringBuilder jsonString = new StringBuilder("<shapes>");
+    public static String xmlBuilder(ArrayList<XMLVisitor> arrayList) throws ParserConfigurationException, IOException, TransformerException, SAXException {
+        StringBuilder xmlStringBuilder = new StringBuilder("<shapes>");
         for (XMLVisitor jv: arrayList){
-            jsonString.append(jv.getRepresentation());
-
+            xmlStringBuilder.append(jv.getRepresentation());
         }
-        jsonString.append("</shapes>");
-        App.xmlToFile(jsonString.toString());
-        System.out.println(jsonString.toString());
-        return jsonString.toString();
+        xmlStringBuilder.append("</shapes>");
+        App.xmlToFile(xmlStringBuilder.toString());
+        //format the xml string to make it more readable
+
+        return xmlStringBuilder.toString();
     }
 
     public static JsonObject importJSON() throws FileNotFoundException {
@@ -75,14 +113,72 @@ public class App
     }
 
     public static void main( String[] args ) throws InterruptedException, FileNotFoundException {
-
-//        System.out.println(App.importJSON());
-
         SwingUtilities.invokeLater(new Runnable() {
-            @Override
             public void run() {
-            GUIHelper.showOnFrame("test");
+                GUIHelper.showOnFrame("test");
+            }
+        });
+
+    }
+
+    private static class PrettyUtils {
+
+        /**
+         * Prettifier pour string de json
+         * source : <a href="https://stackoverflow.com/questions/4105795/pretty-print-json-in-java">...</a>
+         * @param jsonString
+         * @return String
+         */
+        public static String jsonPrettify(String jsonString) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonElement json = JsonParser.parseString(jsonString);
+            return gson.toJson(json);
+        }
+
+        /**
+         * Prettifer pour le fichier xml
+         * source: <a href="https://stackoverflow.com/questions/139076/how-to-pretty-print-xml-from-java">...</a>
+         */
+        public static String xmlStringPrettify(String xmlString) throws ParserConfigurationException, IOException, SAXException, TransformerException {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new InputSource(new StringReader(xmlString)));
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+            transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+            StreamResult result = new StreamResult(new StringWriter());
+            DOMSource source = new DOMSource(document);
+            transformer.transform(source, result);
+            return result.getWriter().toString();
+        }
+
+        public static void strictFileFilter(JFileChooser chooser, String extension) {
+            chooser.setFileFilter(new FileFilter() {
+                public String getDescription() {
+                    return extension;
+                }
+                @Override
+                public boolean accept(File f) {
+                    if (f.isDirectory()) {
+                        return true;
+                    }
+                    String ext = FilenameUtils.getExtension(f.getName());
+                    return ext.equals(extension);
                 }
             });
         }
-	}
+
+    }
+
+    public static class MyRuntimeException extends RuntimeException {
+        public MyRuntimeException(String errorMessage) {
+            Logger logger = Logger.getLogger("MyRuntimeException");
+            logger.log(new LogRecord(Level.FINE, errorMessage));
+        }
+    }
+}
+
